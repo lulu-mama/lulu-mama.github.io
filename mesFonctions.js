28e9135f7d6672e17dcd51ee099ca5a8e65e4146
lulu-mama <65425522+lulu-mama@users.noreply.github.com>
2021-03-03 22:29:20 +0100

    Add files via upload

--- mesFonctions.js
+++ mesFonctions.js
@@ -1,6 +1,6 @@
 function initialize() {
 		// création de la carte et paramétrage général : centre et niveau de zoom
-        var map = L.map('mapid').setView([48.862162, 2.345818], 12);
+        var map = L.map('mapid').setView([47.55, -1.5333], 12);
  
 		// création d'une couche "osmLayer"
         var osmLayer = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -11,19 +11,38 @@ function initialize() {
 		// la couche "osmLayer" est ajoutée à la carte		
         map.addLayer(osmLayer);
 		
-
-		// création d'une couche geoJson qui appelle le fichier "arrondissement.geojson"			
+		/* // création d'une couche geoJson qui appelle le fichier "arrondissement.geojson"			
+		var arrondissement = $.getJSON("arrondissement.geojson",function(dataArrondissement)
+					{L.geoJson( dataArrondissement, 
+						{style: function(feature)
+							{	
+							// paramétrage de la symbologie de la couche "arrondissement"
+							return { color: "#046380", weight: 1, fillColor: '#4BB5C1', fillOpacity: .1 };
+							},
+		onEachFeature: function( feature, layer )
+				{
+				// paramétrage de la popup de la couche "arrondissement"
+				if ([1, 4, 5, 6, 7, 8, 12, 13, 15, 16, 20].includes(feature.properties.c_ar)){ 
+					layer.bindPopup( "<b><u>Touche la Seine</u></b>")
+				}else{
+					layer.bindPopup( "<b><u>Touche pas la Seine</u></b>")
+				}
+				}
+		}).addTo(map);
+		}); */
+		
+		// création d'une couche geoJson qui appelle le fichier "Cadastre_Nantes_44100.geojson"			
 		var parcelle = $.getJSON("Cadastre_Nantes_44100.geojson",function(dataParcelle)
 					{L.geoJson( dataParcelle, 
 						{style: function(feature)
 							{	
 							// paramétrage de la symbologie de la couche "arrondissement"
-							return { color: "#046380", weight: 0, fillColor: '#4BB5C1', fillOpacity: 0 };
+							return { color: "#046380", weight: 1, fillColor: '#4BB5C1', fillOpacity: .05 };
 							},
 		onEachFeature: function( feature, layer )
 				{
 				// paramétrage de la popup de la couche "parcelle"
-				if (feature.properties.numero % 2 == 0)){ 
+				if (feature.properties.numero % 2 == 0){ 
 					layer.bindPopup( "<b><u>Numero pair</u></b>")
 				}else{
 					layer.bindPopup( "<b><u>Numero impair</u></b>")
